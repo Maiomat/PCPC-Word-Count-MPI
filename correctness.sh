1fd18a1868882688bd5ce36465e8d10375f43d6b
Matteo Maiorano <maiomat@hotmail.it>
2023-05-29 12:54:46 +0200

    update dependencies .sh file

--- correctness.sh
+++ correctness.sh
@@ -2,20 +2,21 @@ mpirun -np 2 --allow-run-as-root ./src/word_count.out ./words
 
 cp words_counted.txt twoprocess.txt
 
+sort -o twoprocess.txt twoprocess.txt
+sort -o words_counted.txt words_counted.txt
+
 for i in {5..100..5}
 do
   echo "Checking with $i processors"
 
    mpirun -np $i --allow-run-as-root ./src/word_count.out ./words 
 
-  if [[ $(diff <(sort twoprocess.txt) <(sort words_counted.txt)) ]]; then
+   sort -o words_counted.txt words_counted.txt
+
+  if ! cmp -s twoprocess.txt words_counted.txt; then
         echo "Differences found."
     else
         echo "No differences found."
     fi
 
 done
-
-
-
-
